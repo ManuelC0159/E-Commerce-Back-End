fcf32a7ab117152f0b8385294be7d0974c0a0cc5
Manuel Camacho <manuelcamacho0159@gmail.com>
2023-02-15 11:05:36 -0500

    added routes to category-routes.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+ Product.belongsTo(Category, {
+  foreignKey: 'catergory_id'
+ });
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+  Product.belongsToMany(Tag,{
+    foreignKey: 'product_id'
+  })
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
